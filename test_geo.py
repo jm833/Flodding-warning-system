5a1bdcc29cb0ac852532ddbde2040aa19224bfbc
jm833 <qm220@cam.ac.uk>
2022-02-09 20:18:32 +0000

    Final change

--- test_geo.py
+++ test_geo.py
@@ -1,9 +1,32 @@
-
+from floodsystem.geo import *
+from floodsystem.station import MonitoringStation
 from unittest import result
-from floodsystem.geo import rivers_by_station_number
-from floodsystem.geo import rivers_with_station
-from floodsystem.geo import stations_by_river
 from floodsystem.stationdata import build_station_list
+
+def test_stations_by_distance():
+    station1 = MonitoringStation("s id", "m id","A station", (3.0,4.0),(0.0,1.0),"A river","A town")
+    station2 = MonitoringStation("s id", "m id","B station", (6.0,8.0),(0.0,1.0),"B river","B town")
+
+    stations = [station1,station2]
+    sorted_station_by_distance = stations_by_distance(stations,(0.0,0.0))
+
+    assert sorted_station_by_distance[0][0]== station1
+   
+    
+test_stations_by_distance() 
+
+
+def test_stations_within_radius():
+    station1 = MonitoringStation("s id", "m id","A station", (0.0,0.1),(0.0,1.0),"A river","A town")
+    station2 = MonitoringStation("s id", "m id","B station", (0.1,0.2),(0.0,1.0),"B river","B town")
+    station3 = MonitoringStation("s id", "m id","C station", (9.0,9.0),(0.0,1.0),"C river","C town")
+   
+    stations_within_r = stations_within_radius([station1,station2,station3],(0.0,0.0),100)
+
+    assert stations_within_r == [station1,station2] or [station2,station1]
+
+test_stations_within_radius()
+
 
 
 def test_rivers_with_station():
@@ -79,5 +102,3 @@ def test_rivers_by_station_number():
     # check the result is sorted by number of stations
     for i in range(x-1):
         assert r[i][1] >= r[i+1][1]
-
-
