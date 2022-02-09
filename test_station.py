27aeed72c81249517477bb6be133a6ed274f33db
xiaorui <xz463@cam.ac.uk>
2022-02-09 16:10:49 +0000

    test_station

--- test_station.py
+++ test_station.py
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: MIT
 """Unit test for the station module"""
 
-from floodsystem.station import MonitoringStation
+from floodsystem.station import *
 
 
 def test_create_monitoring_station():
@@ -25,3 +25,27 @@ def test_create_monitoring_station():
     assert s.typical_range == trange
     assert s.river == river
     assert s.town == town
+
+def test_typical_range_consistent():
+    #Create 3 stations, with last two having inconsistent data
+    station1 = MonitoringStation("s id", "m id","some station", (1.0,2.2),(-2.3,3.4),"river x","my town")
+    station2 = MonitoringStation("s id", "m id","some station", (1.0,2.2),(0.0,0.0),"river x","my town")
+    station3 = MonitoringStation("s id", "m id","some station", (1.0,2.2),(1.0,0.5),"river x","my town")
+
+    assert(station1.typical_range_consistent() is True)
+    assert(station2.typical_range_consistent() is False)
+    assert(station3.typical_range_consistent() is False)
+
+
+def test_inconsistent_typical_range_stations():
+    #Create 3 stations, with last two having inconsistent data
+    station1 = MonitoringStation("s id", "m id","some station", (1.0,2.2),(-2.3,3.4),"river x","my town")
+    station2 = MonitoringStation("s id", "m id","some station", (1.0,2.2),(0.0,0.0),"river x","my town")
+    station3 = MonitoringStation("s id", "m id","some station", (1.0,2.2),(1.0,0.5),"river x","my town")
+    station_list = [station1,station2,station3]
+
+    assert(inconsistent_typical_range_stations[station_list]==[station2.name,station3.name])
+
+
+
+    
