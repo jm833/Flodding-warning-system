16c16b3d6e38c0b5488b763281345a8d1e5809cf
xiaorui <xz463@cam.ac.uk>
2022-02-25 22:35:48 +0000

    2E

--- Task2E.py
+++ Task2E.py
@@ -1,21 +1,21 @@
 from floodsystem.plot import *
-from floodsystem.flood import stations_highest_rel_level
-from floodsystem.stationdata import build_station_list
+from floodsystem.flood import *
+from floodsystem.stationdata import *
+
+def task2E():
+    stations = build_station_list()
+
+    #produce the x and y variable: the 5 most risky rivers and their levels
+    risky_stations = stations_highest_rel_level(stations,5)
+    risky_station_objects = [i[0] for i in risky_stations]
 
 
-stations = build_station_list()
+    #plots the water levels over the past 10 days for the 5 stations at which the current relative water level is greatest.
+    for station in risky_station_objects:
+        dates, levels = fetch_measure_levels(station.measure_id, dt = datetime.timedelta(days=10))
 
-#produce the x and y variable: the 5 most risky rivers and their levels
-data= stations_highest_rel_level(stations,5)
-station = []
-station.append(data[i][0] for i in range(5))
+        plot_water_levels(station,dates,levels)
+        plt.show()
 
-#plots the water levels over the past 10 days for the 5 stations at which the current relative water level is greatest.
-for i in stations:
-    for s in station:
-        if s == i.name:
-            dates, levels = fetch_measure_levels(i.measure_id, dt = timedelta(10))
-
-            plot_water_levels(station,dates,levels)
-
-
+if __name__ == "__main__":
+    task2E()
