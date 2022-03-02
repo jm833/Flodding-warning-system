40846d3063a132ee568bf37af54f650bf661c009
jm833 <qm220@cam.ac.uk>
2022-03-02 19:50:28 +0000

    Flood warning finished

--- Warning.py
+++ Warning.py
@@ -3,18 +3,12 @@ from floodsystem.stationdata import build_station_list
 
 def run():
     data = build_station_list()
-
-    for station in data:
-        try:
-            print(polynomial_derivatives(station)[0])
-            print(polynomial_derivatives(station)[1])
-        except(TypeError,KeyError):
-            #print(0,0)
-            pass
-            
+    calculate_kvalue(data)
+   
+    
         
 
-    # print(issue_warning(data))
+   
 
 
 if __name__ == "__main__":
--- floodsystem/warning_system.py
+++ floodsystem/warning_system.py
@@ -48,60 +48,41 @@ def polynomial_derivatives(station):
             pass
 
       
-     
-        
-        
-        
-
-
-      
     
         
 
 
-def obtain_rwl(station):
-    rwl = station.consistent_relative_water_level()
-
-    return rwl
-
 def calculate_kvalue(stations):
-    data = build_station_list()
+    update_water_levels(stations)
 
     valid_rivers = []
 
     for station in stations:
         if station.typical_range_consistent():
             valid_rivers.append(station)
-    
-    k = dict()
 
     for v in valid_rivers:
-        a = obtain_rwl(v)
-        b = (polynomial_derivatives(v)[0])
-        c = (polynomial_derivatives(v)[1])
-        kvalue = 0.63 * a + 0.36 * b + 0.01 * c
-        k[v.name] = kvalue
-    
-    return type(a)
+        try:
+            name = v.name
+            a = v.relative_water_level()
+            b = (polynomial_derivatives(v)[0])
+            c = (polynomial_derivatives(v)[1])
+            kvalue = 0.63 * a + 0.36 * b + 0.01 * c
+            # print(kvalue)
 
-def issue_warning(stations):
-    k = calculate_kvalue(stations)
-    alert = None
+            if kvalue >= 0.6:
+                alert = name + "(green flooding alert)"
+            elif kvalue >= 0.8:
+                alert = name + "(yellow flooding alert)"
+            elif kvalue >= 1:
+                alert = name + "(red flooding alert)"
+            else:
+                continue
 
-    warning = dict()
+            print(alert)
 
-    for key, value in k.items():
-        if value >= 1.5:
-            alert = key + "(green flooding alert)"
-            warning[key] = alert
-        elif value >= 1.9:
-            alert = key + "(yellow flooding alert)"
-            warning[key] = alert
-        elif value >= 2.2:
-            alert = key + "(red flooding alert)"
-            warning[key] = alert
-    
-    return warning
+        except(TypeError,KeyError):
+            pass
 
 
 
